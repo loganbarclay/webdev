64c6ba0a2dfbf3666ad5292de3801c3664a056a1
Logan <loganbarclay@u.boisestate.edu>
2016-09-27 22:06:11 -0600

    links

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
 <?php include_once( "start.html" ); ?>
+<?php include_once( "rentals.html" ); ?>
+<?php include_once( "home.html" ); ?>
+<?php include_once( "sales.html" ); ?>
+<?php include_once( "repairs.html" ); ?>
+<?php include_once( "contact.html" ); ?>
