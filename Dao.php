a9cb3267d73442b5ee36d1208b14511759d5884f
Logan <loganbarclay@u.boisestate.edu>
2016-11-09 16:07:36 -0700

    logins

--- Dao.php
+++ Dao.php
@@ -1,10 +1,10 @@
 <?php
 
 class Dao {
-             private $host = "us-cdbr-east-04.cleardb.com";
-             private $db = "heroku_f3d6b64b4b5dc57";
-             private $user = "be7813156b6434";
-	     private $pass = "b7cb13a4";
+             private $host = "localhost";
+             private $db = "tables";
+             private $user = "root";
+	     private $pass = "root";
 	     public function getConnection () {
                   return
                    new PDO("mysql:host={$this->host};dbname={$this->db}", $user, $pass);
