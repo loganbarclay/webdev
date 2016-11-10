ec15ddb67a62a3eda59586bd6b7cea53ca036d00
Logan <loganbarclay@u.boisestate.edu>
2016-11-10 12:11:36 -0700

    log

---
+++ action_page.php
@@ -0,0 +1,19 @@
+<?php
+
+var_dump($_POST);
+$uname = $psw = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$uname = test_input($_POST["uname"]);
+	$psw = test_input($_POST["psw"]);
+}
+
+function test_input($data){
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+
+?>
