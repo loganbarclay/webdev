a08f407ec5249391a187dfab8f1b7d6c72ff567d
Logan <loganbarclay@u.boisestate.edu>
2016-11-10 13:27:14 -0700

    log

--- action_page.php
+++ action_page.php
@@ -6,14 +6,23 @@ $valid = true;
 
 if(strlen($uname) <= 0 || strlen($uname) > 40) {
 	$unameError = "Username is required.  Must be less than 40 characters.";
+	$valid = false;
 }
 if(strlen($psw) <= 0 || strlen($uname) > 40) {
 	$unameError = "Password is required.  Must be less than 40 characters.";
+	$valid = false;
 }
 
-
 ?>
- 
+<?php
+if($valid == true) {
+	header('Location: home.html');
+}
+else {
+	header('Location: start.html');
+}
+?>
+
 <p>Username: <?= htmlspecialchars($uname) ?></p>
 <?php if(isset($unameError))  { ?>
 	<span id= "unameError" class="error"><?= $unameError ?> </span>
