3ef2a80e120054dbe1c8001677941112d3df561e
Logan <loganbarclay@u.boisestate.edu>
2016-11-10 13:13:20 -0700

    log

--- action_page.php
+++ action_page.php
@@ -1,9 +1,8 @@
 <?php
 
-
-
 $uname = $_POST['uname']; 
 $psw = $_POST['psw'];
+$valid = true;
 
 if(strlen($uname) <= 0 || strlen($uname) > 40) {
 	$unameError = "Username is required.  Must be less than 40 characters.";
@@ -11,19 +10,28 @@ if(strlen($uname) <= 0 || strlen($uname) > 40) {
 if(strlen($psw) <= 0 || strlen($uname) > 40) {
 	$unameError = "Password is required.  Must be less than 40 characters.";
 }
+
+
 ?>
-
-<p>User name: <?= htmlspecialchars($uname)?><p>
-<?php if(isset($unameError))  { ?>
-	<span id= "unameError" class="error"><?= $unameError ?> </span>
-<?php  } ?>
-
-<p> Password: <?= htmlspecialchars($psw)?><p>
 <?php if(isset($unameError))  { ?>
 	<span id= "unameError" class="error"><?= $unameError ?> </span>
 <?php  } ?>
 
 
+<?php if(isset($unameError))  { ?>
+	<span id= "unameError" class="error"><?= $unameError ?> </span>
+<?php  } ?>
+
+<?php
+// if valid, redirect to page
+if($valid == true) {
+	header(*Location: index.php;
+}	
+
+// in not vaild, redirect to form
+
+
+?>
 
 
 
