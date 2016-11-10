ea32df0e0bbbdaf49d8a94a6cb9d8c6ae4ccdf33
Logan <loganbarclay@u.boisestate.edu>
2016-11-10 12:15:35 -0700

    log

--- action_page.php
+++ action_page.php
@@ -5,6 +5,13 @@ var_dump($_POST);
 $uname = $_POST['uname']; 
 $psw = $_POST['psw'];
 
+
+
+?>
+
+<p>User name: <?= $uname?><p>
+<p> Password: <?= $psw?><p>
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$uname = test_input($_POST["uname"]);
 	$psw = test_input($_POST["psw"]);
