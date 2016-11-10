42b6e09a52b43e652b33c1f7fbff4afc168965ed
Logan <loganbarclay@u.boisestate.edu>
2016-11-10 12:59:33 -0700

    log

--- action_page.php
+++ action_page.php
@@ -5,7 +5,7 @@ var_dump($_POST);
 $uname = $_POST['uname']; 
 $psw = $_POST['psw'];
 
-if(strlen($uname) <= 0 || strlen($uname) > 40 {
+if(strlen($uname) <= 0 || strlen($uname) > 40) {
 	$unameError = "Username is required.  Must be less than 40 characters.";
 }
 
