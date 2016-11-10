e2f6098d908d0e83748c2ded5ac0ec0a542f46c0
Logan <loganbarclay@u.boisestate.edu>
2016-11-10 13:22:15 -0700

    log

--- action_page.php
+++ action_page.php
@@ -13,18 +13,19 @@ if(strlen($psw) <= 0 || strlen($uname) > 40) {
 
 
 ?>
-<?php
- // if valid, redirect to page
- if($valid == true) {
-	  header('Location: start.html');
-	   }
+ 
 
-// in not vaild, redirect to form
- else{
-	 header('Location: home.html');
- }
 
-?>
+
+
+
+
+
+
+
+
+
+
 <p>Username: <?= htmlspecialchars($uname) ?></p>
 <?php if(isset($unameError))  { ?>
 	<span id= "unameError" class="error"><?= $unameError ?> </span>
